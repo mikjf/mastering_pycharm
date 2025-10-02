d92d6d72024176564ddeec0ea8ba47b6f8549a1b
Mik <michael.jean.flury@gmail.com>
2025-10-02 15:34:50 +0200

    Add `colorama` dependency and implement basic episode title finder
    - Introduced `colorama` >=0.4.6 as a project dependency.
    - Updated `requirements.piptools`, `requirements.txt`, and `uv.lock` to include `colorama`.
    - Enhanced `first_app.py` to include functionality for retrieving and displaying episode titles from Talk Python FM.

--- first_app.py
+++ first_app.py
@@ -1,1 +1,31 @@
-print('yolo')
+import httpx
+from colorama import Fore
+
+def main():
+    print('-' * 60)
+    print('Talk Python title finder')
+    print('-' * 60)
+
+    num = get_number()
+    title = get_title(num)
+
+    print(f'The title for episode {Fore.GREEN}{num} is {Fore.YELLOW}{title}')
+
+
+def get_number():
+    return int(input("Enter the number of the episode: "))
+
+
+def get_title(episode_number: int) -> str:
+    # https://talkpython.fm/episodes/show/492/great-tables.title
+    url = f'https://talkpython.fm/episodes/show/{episode_number}/any-text-here.title'
+
+    resp = httpx.get(url)
+    resp.raise_for_status()
+
+    return resp.text.strip()
+
+
+
+if __name__ == '__main__':
+    main()
