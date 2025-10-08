f10725435276d09cea3091bf28f5d5bea91918dc
Mik <michael.jean.flury@gmail.com>
2025-10-08 11:12:31 +0200

    Add `Wizard Battle` game in `05_the_editor`
    - Implemented a new interactive text-based game where a wizard battles against various creatures, including dragons, small animals, and other wizards.
    - Added `actors.py` defining classes for `Creature`, `Wizard`, `SmallAnimal`, and `Dragon` with respective behaviors and attributes.
    - Created `program.py` as the game's main entry point with logic for user inputs, creature interactions, and victory conditions.
    - Removed unused `requirements.txt` from `04_code_with_me_podcast`.

---
+++ 05_the_editor/bad_wizard/actors.py
@@ -0,0 +1,79 @@
+import random
+
+
+class Creature:
+    def __init__(self, name, the_level):
+        self.name = name
+        self.level = the_level
+
+    def __repr__(self):
+        return "Creature: {} of level {}".format(
+            self.name, self.level
+        )
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Wizard(Creature):
+
+    def attack(self, creature):
+        """
+        Executes an attack from the wizard against a given creature. The method
+        involves rolling defensive rolls for both the wizard and the creature
+        to determine the outcome of the attack. A successful attack happens if
+        the wizard's defensive roll is greater than or equal to the creature’s
+        defensive roll.
+
+        Args:
+            creature: The opponent in the attack. It is expected to have a
+                      `name` attribute and a `get_defensive_roll` method.
+
+        Returns:
+            bool: True if the wizard triumphs over the creature, otherwise False.
+        """
+        print("The wizard {} attacks {}!".format(
+            self.name, creature.name
+        ))
+
+        my_roll = self.get_defensive_roll()
+        creature_roll = creature.get_defensive_roll()
+
+        print("You roll {}...".format(my_roll))
+        print("{} rolls {}...".format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print("The wizard has handily triumphed over {}".format(creature.name))
+            return True
+        else:
+            print("The wizard has been DEFEATED!!!")
+            return False
+
+    def wake_up(self):
+        print(f"The wizard {self.name} awakens!")
+
+
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
+        self.breaths_fire = breaths_fire
+        self.scaliness = scaliness
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        # fire_modifier = None
+        # if self.breaths_fire:
+        #     fire_modifier = 5
+        # else:
+        #     fire_modifier = 1
+        # fire_modifier = VALUE_IF_TRUE if SOME TEST else VALUE IF FALSE
+        fire_modifier = 5 if self.breaths_fire else 1
+        scale_modifier = self.scaliness / 10
+
+        return base_roll * fire_modifier * scale_modifier
